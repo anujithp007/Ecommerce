0949f00441ee22638353719ab8191afc925f752e
anujithp007 <anujithp007@gmail.com>
2024-03-29 10:57:37 +0530

    last

--- e-com/src/components/Navi.jsx
+++ e-com/src/components/Navi.jsx
@@ -14,7 +14,7 @@ const Navi = () => {
     <div>
       <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <a  className="flex items-center space-x-3 rtl:space-x-reverse">
             <span className="self-center text-2xl font-semibold text-orange-600  whitespace-nowrap dark:text-white">E-Buy</span>
           </a>
           <button className="block md:hidden" onClick={toggleMenu}>
